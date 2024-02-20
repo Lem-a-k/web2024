2bc8da93463573a4a00abca49954f29266ec6353
west1 <aaa>
2024-02-20 17:24:26 +0400

    аватар версия без wtf

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from flask import Flask, url_for, request, render_template
 
 app = Flask(__name__)
 
-params = {'user_name': 'Вася',
-          }
+settings = {'user_name': 'Вася',
+            }
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html", css_url=f"{url_for('static', filename='css/style.css')}",
-                           title="Главная страница", user_name=params.get('user_name', 'Аноним'))
+                           title="Главная страница", user_name=settings.get('user_name', 'Аноним'))
 
 
 @app.route('/test')
@@ -30,90 +30,28 @@ def return_sample_page():
     return render_template('sample_page.html', title='Особая страница', elems=elems)
 
 
-@app.route('/form_sample', methods=['POST', 'GET'])
-def form_sample():
-    if request.method == 'GET':
-        return f'''<!doctype html>
-                        <html lang="en">
-                          <head>
-                            <meta charset="utf-8">
-                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                            <link rel="stylesheet"
-                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                            crossorigin="anonymous">
-                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                            <title>Пример формы</title>
-                          </head>
-                          <body>
-                            <h1>Форма для регистрации в суперсекретной системе</h1>
-                            <div>
-                                <form class="login_form" method="post">
-                                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Введите адрес почты" name="email">
-                                    <input type="password" class="form-control" id="password" placeholder="Введите пароль" name="password">
-                                    <div class="form-group">
-                                        <label for="classSelect">В каком вы классе</label>
-                                        <select class="form-control" id="classSelect" name="class">
-                                          <option>8</option>
-                                          <option>9</option>
-                                          <option>10</option>
-                                          <option>11</option>
-                                        </select>
-                                     </div>
-                                    <div class="form-group">
-                                        <label for="about">Немного о себе</label>
-                                        <textarea class="form-control" id="about" rows="3" name="about"></textarea>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="photo">Приложите фотографию</label>
-                                        <input type="file" class="form-control-file" id="photo" name="file">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="form-check">Укажите пол</label>
-                                        <div class="form-check">
-                                          <input class="form-check-input" type="radio" name="sex" id="male" value="male" checked>
-                                          <label class="form-check-label" for="male">
-                                            Мужской
-                                          </label>
-                                        </div>
-                                        <div class="form-check">
-                                          <input class="form-check-input" type="radio" name="sex" id="female" value="female">
-                                          <label class="form-check-label" for="female">
-                                            Женский
-                                          </label>
-                                        </div>
-                                    </div>
-                                    <div class="form-group form-check">
-                                        <input type="checkbox" class="form-check-input" id="acceptRules" name="accept">
-                                        <label class="form-check-label" for="acceptRules">Готов быть добровольцем</label>
-                                    </div>
-                                    <button type="submit" class="btn btn-primary">Записаться</button>
-                                </form>
-                            </div>
-                          </body>
-                        </html>'''
-    elif request.method == 'POST':
-        try:
-            print(request.form['email'])
-            print(request.form['password'])
-            print(request.form['class'])
-            print(request.form['file'])
-            print(request.form['about'])
-            print(request.form['accept'])
-            print(request.form['sex'])
-        except Exception as e:
-            print(e)
-        return "Форма отправлена"
+@app.route('/avatar', methods=['POST', 'GET'])
+def avatar():
+    if request.method == 'POST':
+        f = request.files['file']
+        settings["avatar_file"] = f.filename
+        f.save(f'static/img/{f.filename}')
+        print(settings["avatar_file"])
+
+    params = {'title': 'Выбор аватара'}
+    if 'avatar_file' in settings:
+        params['avatar'] = url_for('static', filename=f"img/{settings['avatar_file']}")
+    return render_template("avatar.html", **params)
 
 
 @app.route('/test_carousel')
 def return_carousel():
-    if 'pics' not in params:
-        params['pics'] = [(f"{url_for('static', filename='img/1.jpeg')}", "first"),
-                          (f"{url_for('static', filename='img/2.jpeg')}", "second"),
-                          (f"{url_for('static', filename='img/3.jpeg')}", "third")
-                          ]
-    return render_template('test_carousel.html', title='Карусель', pics=params['pics'])
+    if 'pics' not in settings:
+        settings['pics'] = [(f"{url_for('static', filename='img/1.jpeg')}", "first"),
+                            (f"{url_for('static', filename='img/2.jpeg')}", "second"),
+                            (f"{url_for('static', filename='img/3.jpeg')}", "third")
+                            ]
+    return render_template('test_carousel.html', title='Карусель', pics=settings['pics'])
 
 
 if __name__ == '__main__':
